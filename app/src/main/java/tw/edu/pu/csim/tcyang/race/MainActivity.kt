49d2c67c88996be291c92c04f190cd3b3d3c060d
chason-cyber <chason9508@gmail.com>
2025-11-10 14:24:14 +0800

    第一個版本 橫式螢幕

---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/race/MainActivity.kt
@@ -0,0 +1,47 @@
+package tw.edu.pu.csim.tcyang.race
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import tw.edu.pu.csim.tcyang.race.ui.theme.RaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
+
+
+
+
+
+        setContent {
+            RaceTheme {
+                GameScreen(message = "橫式螢幕，隱藏狀態列" )
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier =  modifier
+    )
+}
+
